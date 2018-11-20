4a8a44c9b5f56bff4c0b8c5b7c32584d3e3b7202
MacZel <maciejz4@hotmail.com>
2018-11-20 21:08:31 +0100

    Create prob_1.js

---
+++ js/prob_1.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = (nums, target) => {
+    let num_obj = {}
+    for (i=0; i < nums.length; i++) {
+        num_obj[nums[i]] = i
+    }
+    
+    let res = []
+    for (i=0; i < nums.length; i++) {
+        let result = target - nums[i]
+        
+        if (result in num_obj && num_obj[result] != i) {
+            res.push(i, num_obj[result])
+            
+            return res
+        }
+    }
+    return res
+}
